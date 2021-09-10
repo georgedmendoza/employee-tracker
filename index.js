d276072b8a6fc97b4a154d148b2774ff3fc920d2
georgedmendoza <georgedmendoza13@gmail.com>
2021-09-09 21:46:26 -0500

    show all

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const db = require('./db/connection');
+const Option = require('./lib/Department');
 
 db.connect(err => {
     if(err) throw err;
@@ -18,11 +19,27 @@ const OptionPrompt = () => {
         }
     ])
     .then(selected => {
-        if (selected.view) {
-            
+        if (selected.view === 'View All Departments') {
+            console.log('all dep');
+            // return Option.departments;
+            showAll();
         }
+        else if(selected.view === 'View All Roles') {
+            console.log('roles');
+        }
+        // console.log(selected.view);
+    })
+}
+
+const showAll = () => {
+    const sql = `SELECT name AS department_name, id as department_id FROM DEPARTMENT` 
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.table(result);
     })
 }
 
 OptionPrompt()
-    .then(choiceData => console.log(choiceData));
+    .then(choiceData => {
+        console.log(choiceData);
+    });
