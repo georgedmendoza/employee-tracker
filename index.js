65823d65ab4925dc5848bc470304be9c73e3878a
georgedmendoza <georgedmendoza13@gmail.com>
2021-09-10 18:19:45 -0500

    recall func

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const cTable = require('console.table');
 const db = require('./db/connection');
 const option = require('./lib/queryCalls');
 
-// db.connect(err => {
-//     if(err) throw err;
-//     console.log('Database connected.');
-// });
 
 const OptionPrompt = () => {
     return inquirer.prompt([
@@ -15,16 +11,25 @@ const OptionPrompt = () => {
             name: 'view',
             message: 'What would you like to do?',
             choices: ['View All Departments', 'View All Roles', 'View All Employees',
-                    'Add A Deparment','Add A Role', 'Add An Employee', 'Update A Role']
+                    'Add A Deparment','Add A Role', 'Add An Employee', 'Update A Role', 'NOTHING']
         }
     ])
     .then(selected => {
         if (selected.view === 'View All Departments') {
             option.showAll();
             
+            // OptionPrompt();
+            
         }
         else if(selected.view === 'View All Roles') {
-            option.allRoles();
+            option.allRoles()
+            // console.log(`
+            // =================
+            // SOME TEXT HERE
+            // =================  
+            // `)
+
+            OptionPrompt()
         }
         else if(selected.view === 'View All Employees') {
             option.allEmployees();
@@ -40,19 +45,15 @@ const OptionPrompt = () => {
         }
         else if(selected.view === 'Update A Role') {
             option.updateRole();
-        }
+        } 
+        // else {
+        //     return selected;
+        // }
     })
 }
 
-// const showAll = () => {
-//     const sql = `SELECT name AS department_name, id as department_id FROM DEPARTMENT` 
-//     db.query(sql, (err, result) => {
-//         if(err) throw err;
-//         console.table(result);
-//     })
-// }
-
-OptionPrompt()
-    .then(choiceData => {
-        // console.log(choiceData);
-    });
+// OptionPrompt()
+//     .then(choiceData => {
+//         console.log(typeof choiceData)
+//         console.table(choiceData);
+//     });
